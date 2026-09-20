7d4330912995b5e11682dd5c584776eca6e1671d
josephletobar <162514273+josephletobar@users.noreply.github.com>
2026-09-20 19:55:59 +0000

    Solve: Reshape Matrix

---
+++ problems/0003-reshape-matrix/solution.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
+	#Write your code here and return a python list after reshaping by using numpy's tolist() method
+
+	flat_buffer = []
+
+	for row in a:
+		for i in range(len(row)):
+			flat_buffer.append(row[i])
+
+	if (new_shape[0] * new_shape[1]) < len(flat_buffer):
+		return []
+
+	reshaped_matrix = []
+
+	count = 0
+	for i in range(new_shape[0]):
+		row_buf = []
+		for j in range(new_shape[1]):
+			row_buf.append(flat_buffer[count])
+			count += 1
+
+
+		reshaped_matrix.append(row_buf)
+
+	# print(reshaped_matrix)
+
+	return reshaped_matrix
